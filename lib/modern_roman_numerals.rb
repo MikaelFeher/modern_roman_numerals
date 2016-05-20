84255f67921b84c8deb17edf3a01724b02c93e61
MikaelFeher <mikael.feher@gmail.com>
2016-05-20 15:41:57 +0200

    Added a method and re-arranged the code

--- lib/modern_roman_numerals.rb
+++ lib/modern_roman_numerals.rb
@@ -1,65 +1,66 @@
-puts 'I can help you convert numbers to roman numerals.'
-puts 'Enter a number:'
-num = gets.to_i
-puts ''
-conv_num = num
+def main
+  puts 'I can help you convert numbers to roman numerals.'
+  puts 'Enter a number:'
+  num = gets.to_i
+  puts ''
+  conv_num = num
 
+  conversion_engine conv_num
 
-def modern_roman_numerals(conv_num)
+  puts ''
+  puts ''
+end
 
-result = []
+def conversion_engine(conv_num)
 
+  result = []
 
+  while true
 
-    while true
-
-      case
-      when conv_num >= 1000 then
-        m = conv_num / 1000
-        conv_num -= (m * 1000)
-        result.push('M' * m)
-      when conv_num < 1000 && conv_num >= 500 then
-        m = conv_num / 500
-        conv_num -= (m * 500)
-        result.push('D' * m)
-      when conv_num < 500 && conv_num >= 100 then
-        m = conv_num / 100
-        conv_num -= (m * 100)
-        result.push('C' * m)
-      when conv_num < 100 && conv_num >= 50 then
-        m = conv_num / 50
-        conv_num -= (m * 50)
-        result.push('L' * m)
-      when conv_num < 50 && conv_num >= 10 then
-        m = conv_num / 10
-        conv_num -= (m * 10)
-        result.push('X' * m)
-      when conv_num == 9 then
-        conv_num -= 9
-        result.push('IX')
-      when conv_num < 10 && conv_num >= 5 then
-        m = conv_num / 5
-        conv_num -= (m * 5)
-        result.push('V' * m)
-      when conv_num == 4 then
-        conv_num -= 4
-        result.push('IV')
-      when conv_num < 5 && conv_num >= 1 then
-        m = conv_num / 1
-        conv_num -= (m * 1)
-        result.push('I' * m)
-      end
-
-      break if conv_num == 0
+    case
+    when conv_num >= 1000 then
+      m = conv_num / 1000
+      conv_num -= (m * 1000)
+      result.push('M' * m)
+    when conv_num < 1000 && conv_num >= 500 then
+      m = conv_num / 500
+      conv_num -= (m * 500)
+      result.push('D' * m)
+    when conv_num < 500 && conv_num >= 100 then
+      m = conv_num / 100
+      conv_num -= (m * 100)
+      result.push('C' * m)
+    when conv_num < 100 && conv_num >= 50 then
+      m = conv_num / 50
+      conv_num -= (m * 50)
+      result.push('L' * m)
+    when conv_num < 50 && conv_num >= 10 then
+      m = conv_num / 10
+      conv_num -= (m * 10)
+      result.push('X' * m)
+    when conv_num == 9 then
+      conv_num -= 9
+      result.push('IX')
+    when conv_num < 10 && conv_num >= 5 then
+      m = conv_num / 5
+      conv_num -= (m * 5)
+      result.push('V' * m)
+    when conv_num == 4 then
+      conv_num -= 4
+      result.push('IV')
+    when conv_num < 5 && conv_num >= 1 then
+      m = conv_num / 1
+      conv_num -= (m * 1)
+      result.push('I' * m)
     end
 
-  result.each do |roman|
-      print roman
+    break if conv_num == 0
   end
 
-  puts ''
-  puts ''
+  result.each do |roman|
+    print roman
+  end
 
 end
 
-modern_roman_numerals conv_num
+main
